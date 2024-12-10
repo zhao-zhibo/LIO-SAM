151320b961471de833a8b68017e0d3d20352f275
赵智博 <zbzhao@whu.edu.cn>
2024-12-10 11:03:42 +0800

    修改一些源码中的小错误

--- include/utility.h
+++ include/utility.h
@@ -15,7 +15,7 @@
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
 
-#include <opencv/cv.h>
+#include<opencv2/imgproc.hpp>
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
